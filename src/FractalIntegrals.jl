1fe31a56e70d4a76ce3e91692e1767534a8bd5aa
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-03-08 20:35:17 +0000

    Have things working (suboptimally) with SVectors

--- src/FractalIntegrals.jl
+++ src/FractalIntegrals.jl
@@ -1,5 +1,10 @@
 module FractalIntegrals
 
-# Write your package code here.
+import StaticArrays: SVector, SMatrix, mul!
+import LinearAlgebra: norm, det
+import LinearAlgebra.I as IdMat
+export Similarity
+
+include("AffineMaps.jl")
 
 end
