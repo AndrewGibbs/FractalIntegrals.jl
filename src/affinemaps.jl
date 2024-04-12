c5f051d5f3b027c55b022324a2a5a605dad34c92
Andrew Gibbs <andrew.gibbs@ucl.ac.uk>
2024-04-12 17:44:46 +0100

    Added symmetry group functions

--- src/affinemaps.jl
+++ src/affinemaps.jl
@@ -151,6 +151,7 @@ Base.one(::TranslatingSimilarity{T, V}) where {N, T<:Number, V<:SVector{N,T}} =
 Base.one(::Similarity{T, V, M}) where {N, T<:Number, V<:SVector{N,T}, M<:SMatrix{N,N,T}} = 
     Similarity(one(T), zero(V), one(V), zero(M))
 
+# define the identity automorphism
 function IdentityInvariantMap(T::Type, n::Integer)
     if n==1
         I = OneDimensionalInvariantMap(zero(T), one(T))
@@ -159,9 +160,53 @@ function IdentityInvariantMap(T::Type, n::Integer)
     end
     return I
 end
+IdentityInvariantMap(n::Integer) = IdentityInvariantMap(Float64, n)
 
 function simcompsymmetries(s::AbstractSimilarity, symmetries::AbstractVector{T}) where T<:AbstractInvariantMap
     Achain = [s.A*symmetries[m].A/s.A for m in eachindex(symmetries)]
     # rAchain = [symmetries[m].ρ*Achain[m] for m in eachindex(symmetries)]
     return [T((IdMat-Achain[m])*s.δ + s.ρA*symmetries[m].δ, Achain[m]) for m in eachindex(symmetries)]
 end
+
+## Invariant/automoprhic maps/symmetries - focusing on 2d stuff for now
+
+rotationmatrix2d(θ::T) = SMatrix{2,2,T}([cos(θ) -sin(θ); sin(θ) cos(θ)])
+reflectionmatrix2d(θ::T) = SMatrix{2,2,T}([cos(2θ) sin(2θ); sin(2θ) -cos(2θ)])
+
+function get_group_operations2d(num_rotations::Integer,
+                                reflections::AbstractVector{<:Real},
+                                centre::AbstractVector{T}) where T<:Number
+    δθ = 2π / num_rotations
+    
+    ivmaps = Vector{InvariantMap{T,SVector{2,T},SMatrix{2,2,T}}
+                    }(undef, num_rotations + num_reflections)#[IdentityMap(2) for _=1:(num_rotations+num_reflections)]
+
+    counter = 0
+    for θ in 0:δθ:(2π-δθ)
+        counter += 1
+        rotmat = rotationmatrix2d(θ)
+        ivmaps[counter] = AutomorphicMap(Arot, centre - rotmat * centre)
+    end
+    for ϑ in reflections
+        counter += 1
+        refmat = reflectionmatrix2d(ϑ)
+        ivmaps[counter] = AutomorphicMap(refmat, centre - refmat * centre)
+    end
+    # the union of rotations and reflections gives all compositions
+    return ivmaps
+end
+
+function DihedralGroup( n::Integer;
+                        centre::AbstractVector{<:Number} = [0.0,0.0],
+                        angle_offest::Number = 0.0)
+    if mod(n,2) == 0 # for even n, want to double frequency of reflections, but restrict to [0,π]
+        δθ = π/n
+        reflections = (0:δθ:(π-δθ)) .+ angle_offest
+    else
+        δθ = 2π/n # for odd n, split [0,2π] into n segments
+        reflections = (0:δθ:(2π-δθ)) .+ angle_offest
+    end
+    return get_group_operations2D(n, reflections,centre)
+end
+
+# D₂_in_1D(;centre::Float64=0.0) = [IdentityMap(1), OneDimensionalInvariantMap(-1.0, 2*centre)]
